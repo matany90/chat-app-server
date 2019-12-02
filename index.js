ecdec0a995cae97bee788e5c0daeca681c4b9c82
MATAN\matan <matany90@gmail.com>
2019-12-02 16:57:42 +0200

    middlware

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  })
+
 app.get('/', (req, res) => {
     res.send({ test: 'test'})
 })
