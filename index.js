249198ab05306f2fe1f70f4a5561dc13d228b27d
MATAN\matan <matany90@gmail.com>
2019-12-08 18:11:49 +0200

    init

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  })
+io.on('connection', (socket) => {
+    console.log('new connection')
 
-app.get('/', (req, res) => {
-    res.send({ test: 'test'})
+    socket.on('join', ({ name, room }, errorCb) => {
+        console.log(name, room)
+    })
+
+    socket.on('disconnect', () => console.log('user had left'))
 })
+require('./routes/chatRouter')(app);
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   })
 
-app.listen(PORT);
+server.listen(PORT);
